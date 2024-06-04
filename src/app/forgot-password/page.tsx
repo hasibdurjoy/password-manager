d39bea0908a86df976a9fcd15e9244e75544291e
hasiburrahmandurjoy <hasibur.rahman@vigaet.com>
2024-06-05 01:00:48 +0600

    reset password page

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -1,59 +1,64 @@
-// app/login/page.tsx
 "use client";
 
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import Swal from "sweetalert2";
 
-export default function Login() {
+const page = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const router = useRouter();
 
   const handleLogin = async () => {
-    try {
-      const res = await axios.post("/api/auth/login", { username, password });
-      localStorage.setItem("token", res.data.token);
-      router.push("/");
-    } catch (err: any) {
-      setError(err.response.data.message);
+    if (username) {
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Reset Link send to your email please check",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      setTimeout(() => {
+        router.push("/login");
+      }, 2000);
+    } else {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Email required",
+        showConfirmButton: false,
+        timer: 1500,
+      });
     }
   };
-
   return (
     <div className="flex items-center justify-center h-[90vh]">
       <div className="max-w-md mx-auto p-4">
-        <h1 className="text-2xl font-bold mb-4 text-center">Login</h1>
+        <h1 className="text-2xl font-bold mb-4 text-center">Forgot Password</h1>
         {error && <p className="text-red-500">{error}</p>}
         <input
-          type="text"
+          type="email"
           className="border p-2 w-full mb-4"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-        />
-        <input
-          type="password"
-          className="border p-2 w-full mb-4"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
+          placeholder="Email"
+          required
         />
         <button
           onClick={handleLogin}
           className="bg-blue-500 text-white p-2 rounded w-full"
         >
-          Login
+          Send Reset Link
         </button>
         <div className="text-center mt-4 text-blue-400">
-          <Link href="/register">Don't Have Account? Create account</Link>
-        </div>
-        <div className="text-center mt-4 text-blue-400">
-          <Link href="/forgot-password">Forgot Password</Link>
+          <Link href="/login">Log in</Link>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default page;
